fb30e116127879003630f8396f63dcc750cffb3d
Daniel <d.mccrystal512@gmail.com>
2017-04-01 12:48:21 -0400

    tweaked genetic algorithm

--- source_code/Genetic_Client.py
+++ source_code/Genetic_Client.py
@@ -1,4 +1,4 @@
-import random,
+import random
 import source_code.Utils as utils
 from source_code.Grid import Grid
 
@@ -6,8 +6,12 @@ class Genetic_Client:
 	# Start with an initial population of variable size
 	def __init__(self, population_size):
 		self.generation = 1
-
-		pass
+		self.population = [[None for i in range(2)] for j in range(population_size)]
+		for i in range(population_size):
+			grid = Grid(48)
+			grid.populate_random()
+			self.population[i][0] = grid
+			self.population[i][1] = self.chord_fitness(grid)
 
 	# Determine the fitness of a particular chord specimen
 	def chord_fitness(self, specimen):
@@ -18,7 +22,7 @@ class Genetic_Client:
 
 		# Tier 1 (Individual Columns)
 		last_chord = None
-		tier_1_scores = []
+		scores = []
 		for col in range(specimen.num_notes):
 			# generate list of notes in chord
 			notes_in_col = []
@@ -43,16 +47,42 @@ class Genetic_Client:
 						count += 1
 				ratios[chord] = float(count) / len(notes_in_col)
 			best_ratio = max(ratios)
-			tier_1_scores.append(best_ratio)
+			scores.append(best_ratio)
 			best_chord = possible_chords[ratios.index(best_ratio)]
 			col_chords[col] = best_chord
 			last_chord = best_chord
-		multiplier = utils.geometric_mean(tier_1_scores) * 2
-		print(multiplier)
+		multiplier = (utils.geometric_mean(scores) * 2) ** 4
 		fitness *= multiplier
 
 		# Tier 2 (Chord Changing)
+		chord_sequence = utils.compact_chord_cols(col_chords)
+		scores = [None] * len(chord_sequence)
+		expected = 8
+		sd = 4
+		max_score = utils.norm_pdf(expected, expected, sd)
+		for i in range(len(chord_sequence)):
+			chord = chord_sequence[i]
+			scores[i] = utils.norm_pdf(chord[1], expected, sd) / max_score
 
+		multiplier = utils.geometric_mean(scores) * 2
+		fitness *= multiplier
+
+		# Tier 3 (Overlap)
+		expected = 4
+		sd = 1
+		scores = [None] * specimen.num_notes
+		max_score = utils.norm_pdf(expected, expected, sd)
+		for col in range(specimen.num_notes):
+			count = 0
+			for pitch in range(specimen.note_range):
+				if grid[col][pitch] is not None:
+					count += 1
+			scores[col] = utils.norm_pdf(count, expected, sd)
+		multiplier = utils.geometric_mean(scores) * 2
+		fitness *= multiplier
+
+		# Tier 4 (Key)
+		
 
 		return fitness
 
@@ -61,26 +91,42 @@ class Genetic_Client:
 	# Kill bottom 50%
 	# Repopulate with offspring of top 50%
 	def darwin(self):
+		self.population = sorted(self.population, key=lambda x: x[1])
+		print("Top fitness: " + str(self.population[len(self.population)-1][1]))
+		print("Average fitness: " + str(self.avg_fitness()))
+		survivors = range(int(len(self.population) / 2), len(self.population))
+		deaths = range(int(len(self.population) / 2))
+		offsprings = [self.offspring(self.population[i][0]) for i in survivors]
+		for i in deaths:
+			self.population[i] = [offsprings[i], self.chord_fitness(offsprings[i])]
 		print("Simulated gen " + str(self.generation))
 		self.generation = self.generation + 1
-		pass
+
+	def avg_fitness(self):
+		sum = 0
+		for individual in self.population:
+			sum += individual[1]
+		return sum / len(self.population)
+
+	def best_individual(self):
+		return self.population[len(self.population)-1][0]
 
 	# Generate an offspring of a particular specimen with random mutations
 	def offspring(self, specimen):
 		grid = specimen.grid
-		offspring = [list(col) for col in list(grid)]
-		for pos in range(grid.num_notes):
-			for pitch  in range(grid.note_range):
+		offspring = specimen.copy()
+		for pos in range(specimen.num_notes):
+			for pitch  in range(specimen.note_range):
 				if grid[pos][pitch]:
-					if random.random() < .0001:
-						pos_movement = random.randint(-2, 2)
-						if pos + pos_movement < 0 or pos + pos_movement >= grid.num_notes:
+					if random.random() < .05:
+						pos_movement = random.randint(-4, 4)
+						if pos + pos_movement < 0 or pos + pos_movement >= specimen.num_notes:
 							pos_movement = 0
-						pitch_movement = random.randint(-2, 2)
-						if pitch + pitch_movement < 0 or pitch + pitch_movement >= grid.note_range:
+						pitch_movement = random.randint(-4, 4)
+						if pitch + pitch_movement < 0 or pitch + pitch_movement >= specimen.note_range:
 							pitch_movement = 0
-						new_duration = random.randint(1, 16)
-						if pos + pos_movement + new_duration >= grid.num_notes - 1:
+						new_duration = random.randint(1, 8)
+						if pos + pos_movement + new_duration >= specimen.num_notes - 1:
 							new_duration = 1
 						offspring.remove_note(pos, pitch)
 						offspring.add_note(pos + pos_movement, pitch + pitch_movement, new_duration)
@@ -89,9 +135,11 @@ class Genetic_Client:
 
 
 if __name__ == '__main__':
-	gc = Genetic_Client(1000)
+	gc = Genetic_Client(100)
+	# gc.population[0][0].convert_to_MIDI("../outputs/start.mid")
 	inpt = input("How many generations should I simulate? ")
 	while(inpt != "done"):
 		for i in range(int(inpt)):
 			gc.darwin()
 		inpt = input("How many generations should I simulate? ")
+	gc.best_individual().convert_to_MIDI("../outputs/best.mid")
--- source_code/Grid.py
+++ source_code/Grid.py
@@ -1,14 +1,18 @@
 from midiutil.MidiFile import MIDIFile
 import random
-from source_code.Genetic_Client import Genetic_Client
 import source_code.Utils as utils
 
 class Grid:
-	def __init__(self, length):
-		self.grid = [[None for i in range(24)] for j in range(length)]
+	def __init__(self, length, grid = None):
+		self.grid = grid
+		if grid is None:
+			self.grid = [[None for i in range(24)] for j in range(length)]
 		self.num_notes = len(self.grid)
 		self.note_range = len(self.grid[0])
 		self.lowest_note = 48
+
+	def copy(self):
+		return Grid(self.num_notes, grid = [list(col) for col in list(self.grid)])
 
 	def add_note(self, pos, pitch, duration):
 		self.grid[pos][pitch] = True
@@ -16,10 +20,12 @@ class Grid:
 			self.grid[pos+1+i][pitch] = False
 
 	def remove_note(self, pos, pitch):
-		if not self.grid[pos, pitch]:
+		if not self.grid[pos][pitch]:
 			return
 		duration = 1
-		next_cell = self.grid[pos + duration][pitch]
+		next_cell = None
+		if pos + duration < self.num_notes:
+			next_cell = self.grid[pos + duration][pitch]
 		while next_cell is not None and not next_cell:
 			duration += 1
 			if pos + duration < self.num_notes:
@@ -30,10 +36,10 @@ class Grid:
 
 	def populate_random(self):
 		i = 0
-		while i < 96:
+		while i < self.num_notes:
 			pos = random.randint(0, self.num_notes-1)
 			pitch = random.randint(0, self.note_range-1)
-			duration = random.randint(1, 16)
+			duration = random.randint(1, 8)
 			if self.grid[pos][pitch] is None and pos + duration < self.num_notes:
 				self.add_note(pos, pitch, duration)
 				i += 1
@@ -48,7 +54,9 @@ class Grid:
 			for pitch in range(self.note_range):
 				if self.grid[pos][pitch]:
 					duration = 1
-					next_cell = self.grid[pos + duration][pitch]
+					next_cell = None
+					if pos + duration < self.num_notes:
+						next_cell = self.grid[pos + duration][pitch]
 					while next_cell is not None and not next_cell:
 						duration += 1
 						if pos + duration < self.num_notes:
@@ -58,16 +66,3 @@ class Grid:
 					mf.addNote(0, 0, pitch + self.lowest_note, pos, duration, 80)
 		with open(title, 'wb') as outf:
 			mf.writeFile(outf)
-
-if __name__ == '__main__':
-	g = Grid(96)
-	g.populate_random()
-	'''
-	chord = utils.get_triad(10, 0)
-	for note in chord:
-		g.add_note(0, note, 8)
-	'''
-	g.convert_to_MIDI("../outputs/test.mid")
-	gc = Genetic_Client(1)
-	print(gc.chord_fitness(g))
-	print("done")
